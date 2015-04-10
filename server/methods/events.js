8cffe5c292615bf0822dfeb3ea0202addbb57f9c
WaSa42 <nicolasrouviere6@gmail.com>
2015-04-10 17:59:14 +0200

    Amélioration des méthodes de gestion d'évènements Intégration et design

--- server/methods/events.js
+++ server/methods/events.js
@@ -34,39 +34,46 @@ Meteor.methods({
 
         var event = Events.findOne(values.id);
 
+        var set = {
+            name:       values.name,
+            begin:      values.begin,
+            end:        values.end,
+            price:      values.price,
+            address:    values.address,
+            zipCode:    values['zip-code'],
+            city:       values.city,
+            country:    values.country,
+            link:       values.link,
+            comment:    values.comment
+        };
+
+        if (values.picture) {
+           set.picture =  values.picture;
+        }
+
         Events.update(event._id, {
-            $set: {
-                name:       values.name,
-                begin:      values.begin,
-                end:        values.end,
-                price:      values.price,
-                address:    values.address,
-                zipCode:    values['zip-code'],
-                city:       values.city,
-                country:    values.country,
-                link:       values.link,
-                picture:    values.picture,
-                comment:    values.comment
-            }
+            $set: set
         });
 
-        fs.exists(DIR + event._id, function (exists) {
-            if (exists) {
-                fs.unlink(DIR + event._id, function (err) {
-                    if (err) throw err;
-                });
-            }
-        });
+        if (values.picture) {
+            fs.exists(DIR + event._id, function (exists) {
+                if (exists) {
+                    fs.unlink(DIR + event._id, function (err) {
+                        if (err) throw err;
+                    });
+                }
+            });
 
-        fs.exists(DIR + values.picture, function (exists) {
-            if (exists) {
-                fs.rename(DIR + values.picture, DIR + event._id, function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                });
-            }
-        });
+            fs.exists(DIR + values.picture, function (exists) {
+                if (exists) {
+                    fs.rename(DIR + values.picture, DIR + event._id, function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        }
     },
     removeEvent: function(id) {
         //checkAdmin();
@@ -81,7 +88,7 @@ Meteor.methods({
 
         fs.exists(DIR + event._id, function (exists) {
             if (exists) {
-                fs.unlink(dirPublic + event._id, function (err) {
+                fs.unlink(DIR + event._id, function (err) {
                     if (err) throw err;
                 });
             }
